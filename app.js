41ab60a9e3581884869ef40034bba94d4f03dbd7
perryesar <audrey01>
2016-08-09 16:42:43 -0400

    Updated code changes

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var _= require('underscore');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var todos =[];
+var todoNextId = 1;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,11 +29,11 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handlers
 
@@ -56,5 +59,109 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// var todos = [{
+//   id: 1,
+//   description: 'Meet Fred for Lunch',
+//   completed: false
+// }, {
+//   id: 2,
+//   description: 'Go to market',
+//   completed: false
+// }, {
+//   id: 3,
+//   description: 'Walk the dog',
+//   completed: true
+// }];
+
+app.get('/', function(req,res){
+  res.send('Todo API Root');
+});
+
+app.get('/todos', function(req, res) {
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.where(filteredTodos, {
+            completed: true
+        });
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(filteredTodos, {
+            completed: false
+        });
+    }
+
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+        filteredTodos = _.filter(filteredTodos, function(todo) {
+            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+        });
+    }
+
+    res.json(filteredTodos);
+});
+
+app.get('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {
+        id: todoId
+    });
+
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
+});
+
+app.post('/todos', function(req, res){
+    var body = _.pick(req.body, 'description', 'completed');
+
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+      return res.status(400).send();
+    }
+    body.description = body.description.trim();
+    body.id = todoNextId++;
+
+    todos.push(body);
+    res.json(body);
+});
+
+app.delete('/todos/:id', function(req,res){
+    var todoId = parseInt(req.params.id,10);
+    var matchTodo = _.findWhere(todos, {id: todoId});
+
+    if (!matchTodo){
+        res.status(404).send();
+    } else {
+        todos = _.without(todos, matchTodo);
+        res.json(matchTodo);
+    }
+});
+
+app.put('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+});
 
 module.exports = app;
